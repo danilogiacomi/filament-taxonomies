0d3d15015d00eeb88e8afa79dae835e95fa65315
AlessandroBertozzi <a.bertozz96@gmail.com>
2025-07-02 01:25:42 +0200

    add support to multiple selection

--- src/Forms/Components/TaxonomySelect.php
+++ src/Forms/Components/TaxonomySelect.php
@@ -5,10 +5,19 @@ namespace Net7\FilamentTaxonomies\Forms\Components;
 use Filament\Forms\Components\Select;
 use Net7\FilamentTaxonomies\Models\Taxonomy;
 use Net7\FilamentTaxonomies\Models\EntityTerm;
+use Closure;
 
 class TaxonomySelect extends Select
 {
     protected string $taxonomy;
+    protected Closure|bool $isMultiple = false;
+
+    public function multiple(Closure|bool $condition = true): static
+    {
+        $this->isMultiple = true;
+        parent::multiple($condition);
+        return $this;
+    }
 
     public function taxonomy(string $taxonomy): static
     {
@@ -27,23 +36,60 @@ class TaxonomySelect extends Select
 
         $this->afterStateHydrated(function (Select $component, $state, $record) use ($taxonomy) {
             if ($record) {
-                $existingTerm = $record->entityTerms()
-                    ->where('taxonomy_type', $taxonomy)
-                    ->first();
+                if ($this->evaluate($this->isMultiple)) {
+                    $existingTerms = $record->entityTerms()
+                        ->where('taxonomy_type', $taxonomy)
+                        ->pluck('term_id')
+                        ->toArray();
 
-                if ($existingTerm) {
-                    $component->state($existingTerm->term_id);
+                    $component->state($existingTerms);
+                } else {
+                    $existingTerm = $record->entityTerms()
+                        ->where('taxonomy_type', $taxonomy)
+                        ->first();
+
+                    if ($existingTerm) {
+                        $component->state($existingTerm->term_id);
+                    }
                 }
             }
         });
 
         $this->saveRelationshipsUsing(function (Select $component, $state, $record) use ($taxonomy) {
             if ($record) {
-                $this->saveEntityTerm($record, $taxonomy, $state);
+                if ($this->evaluate($this->isMultiple)) {
+                    $this->saveMultipleEntityTerms($record, $taxonomy, $state);
+                } else {
+                    $this->saveEntityTerm($record, $taxonomy, $state);
+                }
             }
         });
 
         return $this;
+    }
+
+    protected function saveMultipleEntityTerms($record, string $taxonomyType, array $termIds): void
+    {
+        EntityTerm::where('entity_type', get_class($record))
+            ->where('entity_id', $record->id)
+            ->where('taxonomy_type', $taxonomyType)
+            ->delete();
+
+        if (!empty($termIds)) {
+            $data = [];
+            foreach ($termIds as $termId) {
+                $data[] = [
+                    'entity_type' => get_class($record),
+                    'entity_id' => $record->id,
+                    'taxonomy_type' => $taxonomyType,
+                    'term_id' => $termId,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+            }
+
+            EntityTerm::insert($data);
+        }
     }
 
     protected function saveEntityTerm($record, string $taxonomyType, $termId): void
@@ -60,7 +106,6 @@ class TaxonomySelect extends Select
                 ]
             );
         } else {
-            // Remove if no term selected
             EntityTerm::where('entity_type', get_class($record))
                 ->where('entity_id', $record->id)
                 ->where('taxonomy_type', $taxonomyType)
